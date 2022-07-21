6b732c53a7d6693f554b00f359a20a344ecdf297
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2022-07-21 18:51:14 +0300

    explicit types: clear
    :

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,34 @@
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// const circ = (diameter: number) => {
+//   return diameter * Math.PI
+// }
+// console.log(circ(7.5))
+//explicit types
+var character;
+var age;
+var isLoggedIn;
+var people = [];
+people.push('Alice', 'Bob');
+console.log(people);
+//union types
+var mixed = [];
+mixed.push('Hello');
+mixed.push(5);
+mixed.push(true);
+console.log(mixed);
+var uid;
+uid = '123';
+uid = 123;
+//objects
+var person1;
+person1 = {
+    name: 'Charles',
+    age: 24,
+    job: 'Developer'
 };
-console.log(circ(7.5));
+var person2;
+person2 = {
+    name: 'Kyle',
+    age: 30,
+    job: 'Youtuber'
+};
+console.log(person1, person2);
--- sandbox.ts
+++ sandbox.ts
@@ -1,5 +1,46 @@
-const circ = (diameter: number) => {
-  return diameter * Math.PI
+// const circ = (diameter: number) => {
+//   return diameter * Math.PI
+// }
+
+// console.log(circ(7.5))
+
+//explicit types
+let character: string
+let age: number
+let isLoggedIn: boolean
+
+let people: string[] = []
+people.push('Alice', 'Bob')
+console.log(people)
+
+//union types
+let mixed: (string | number | boolean)[] = []
+mixed.push('Hello')
+mixed.push(5)
+mixed.push(true)
+console.log(mixed)
+
+let uid: string | number
+uid = '123'
+uid = 123
+
+//objects
+let person1: object
+person1 = {
+  name: 'Charles',
+  age: 24,
+  job: 'Developer',
 }
 
-console.log(circ(7.5))
+let person2: {
+  name: string
+  age: number
+  job: string
+}
+person2 = {
+  name: 'Kyle',
+  age: 30,
+  job: 'Youtuber',
+}
+
+console.log(person1, person2)
