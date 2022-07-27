431503af6c86c57d65b6426b59157de6f0b63e2b
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2022-07-27 17:55:33 +0300

    functions basics

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -5,63 +5,83 @@
 // console.log(circ(7.5))
 
 //explicit types
-let character: string
-let age: number
-let isLoggedIn: boolean
+// let character: string
+// let age: number
+// let isLoggedIn: boolean
 
-let people: string[] = []
-people.push('Alice', 'Bob')
-console.log(people)
+// let people: string[] = []
+// people.push('Alice', 'Bob')
+// console.log(people)
 
 //union types
-let mixed: (string | number | boolean)[] = []
-mixed.push('Hello')
-mixed.push(5)
-mixed.push(true)
-console.log(mixed)
+// let mixed: (string | number | boolean)[] = []
+// mixed.push('Hello')
+// mixed.push(5)
+// mixed.push(true)
+// console.log(mixed)
 
-let uid: string | number
-uid = '123'
-uid = 123
+// let uid: string | number
+// uid = '123'
+// uid = 123
 
 //objects
-let person1: object
-person1 = {
-  name: 'Charles',
-  age: 24,
-  job: 'Developer',
-}
+// let person1: object
+// person1 = {
+//   name: 'Charles',
+//   age: 24,
+//   job: 'Developer',
+// }
 
-let person2: {
-  name: string
-  age: number
-  job: string
-}
-person2 = {
-  name: 'Kyle',
-  age: 30,
-  job: 'Youtuber',
-}
+// let person2: {
+//   name: string
+//   age: number
+//   job: string
+// }
+// person2 = {
+//   name: 'Kyle',
+//   age: 30,
+//   job: 'Youtuber',
+// }
 
-console.log(person1, person2)
+// console.log(person1, person2)
 
 // any type
-let years: any = 25
-console.log(years)
-years = true
-console.log(years)
-years = 'hello'
-console.log(years)
+// let years: any = 25
+// console.log(years)
+// years = true
+// console.log(years)
+// years = 'hello'
+// console.log(years)
 
-let mixedArray: any[] = []
-mixedArray.push('hello')
-mixedArray.push(true)
-mixedArray.push(34)
-console.log(mixedArray)
+// let mixedArray: any[] = []
+// mixedArray.push('hello')
+// mixedArray.push(true)
+// mixedArray.push(34)
+// console.log(mixedArray)
 
-let object: { name: any; age: any }
-object = { name: 'Alice', age: 20 }
-object = { name: true, age: 'hello' }
-console.log(object)
+// let object: { name: any; age: any }
+// object = { name: 'Alice', age: 20 }
+// object = { name: true, age: 'hello' }
+// console.log(object)
 
-console.log('from the src folder')
+// console.log('from the src folder')
+
+let greet: Function
+
+greet = () => {
+  console.log('Hello, world')
+}
+
+let add = (a: number, b: number, c: number | string = 10): void => {
+  console.log(a + b)
+  console.log(c)
+}
+
+add(5, 10, '20')
+
+let minus = (a: number, b: number): number => {
+  return a - b
+}
+
+let result = minus(10, 7)
+console.log(result)
