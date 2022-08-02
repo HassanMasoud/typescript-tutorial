61e60eb2b8635879f07276947ca4318f44fa2e28
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2022-08-02 23:32:20 +0300

    interfaces

--- public/app.js
+++ public/app.js
@@ -1,18 +1,17 @@
-import { Invoice } from './classes/Invoice.js';
-const invOne = new Invoice('Hassan', 'website work', 300);
-const invTwo = new Invoice('Tracy', 'marketing work', 500);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach((invoice) => {
-    console.log(invoice.format());
-});
-const form = document.querySelector('.new-item-form');
-const type = document.querySelector('#type');
-const tofrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
-});
+"use strict";
+// import { Invoice } from './classes/Invoice.js'
+const me = {
+    name: 'Hassan',
+    age: 28,
+    speek(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent ' + amount);
+        return amount;
+    },
+};
+const greetPerson = (person) => {
+    console.log('Hello', person.name);
+};
+greetPerson(me);
--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,50 @@
-import { Invoice } from './classes/Invoice.js'
+// import { Invoice } from './classes/Invoice.js'
 
-const invOne = new Invoice('Hassan', 'website work', 300)
-const invTwo = new Invoice('Tracy', 'marketing work', 500)
+// const invOne = new Invoice('Hassan', 'website work', 300)
+// const invTwo = new Invoice('Tracy', 'marketing work', 500)
 
-let invoices: Invoice[] = []
-invoices.push(invOne)
-invoices.push(invTwo)
+// let invoices: Invoice[] = []
+// invoices.push(invOne)
+// invoices.push(invTwo)
 
-invoices.forEach((invoice) => {
-  console.log(invoice.format())
-})
+// invoices.forEach((invoice) => {
+//   console.log(invoice.format())
+// })
 
-const form = document.querySelector('.new-item-form') as HTMLFormElement
+// const form = document.querySelector('.new-item-form') as HTMLFormElement
 
-const type = document.querySelector('#type') as HTMLSelectElement
-const tofrom = document.querySelector('#tofrom') as HTMLInputElement
-const details = document.querySelector('#details') as HTMLInputElement
-const amount = document.querySelector('#amount') as HTMLInputElement
+// const type = document.querySelector('#type') as HTMLSelectElement
+// const tofrom = document.querySelector('#tofrom') as HTMLInputElement
+// const details = document.querySelector('#details') as HTMLInputElement
+// const amount = document.querySelector('#amount') as HTMLInputElement
 
-form.addEventListener('submit', (e: Event) => {
-  e.preventDefault()
-  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber)
-})
+// form.addEventListener('submit', (e: Event) => {
+//   e.preventDefault()
+//   console.log(type.value, tofrom.value, details.value, amount.valueAsNumber)
+// })
+
+// Interfaces
+interface IsPerson {
+  name: string
+  age: number
+  speek(a: string): void
+  spend(a: number): number
+}
+
+const me: IsPerson = {
+  name: 'Hassan',
+  age: 28,
+  speek(text: string): void {
+    console.log(text)
+  },
+  spend(amount: number): number {
+    console.log('I spent ' + amount)
+    return amount
+  },
+}
+
+const greetPerson = (person: IsPerson) => {
+  console.log('Hello', person.name)
+}
+
+greetPerson(me)
