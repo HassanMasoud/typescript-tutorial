ca4e43f2f06b20975effe6847de9528ec22e26cc
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2022-08-02 23:05:49 +0300

    access modifiers

--- public/app.js
+++ public/app.js
@@ -1,9 +1,17 @@
 "use strict";
 class Invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    //   client: string
+    //   details: string
+    //   amount: number
+    //   constructor(c: string, d: string, a: number) {
+    //     this.client = c
+    //     this.details = d
+    //     this.amount = a
+    //   }
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     format() {
         return `${this.client} owes £${this.amount} for ${this.details}`;
